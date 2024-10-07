748bd644634e895f2556086c198894869f8010e7
karun-j <karun.j@turing.com>
2024-10-07 18:03:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,12 @@
-# terraform/main.tf
-
-provider "aws" {
-  region = "us-east-1"  # Change to your preferred region
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Allow web inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Change to a valid AMI ID
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.allow_web.name]
-
-  tags = {
-    Name = "WebServer"
+# Define a simple resource just for testing
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo Hello, Terraform!"
   }
 }
