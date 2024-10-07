3c81ac19173510fba05353cd3023abedc7d92dc5
karun-j <karun.j@turing.com>
2024-10-07 22:47:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,11 @@
+# terraform/main.tf
+
 terraform {
   backend "local" {
     path = "terraform.tfstate"
   }
 }
 
-# Define a simple resource just for testing
 resource "null_resource" "example" {
   provisioner "local-exec" {
     command = "echo Hello, Terraform!"
